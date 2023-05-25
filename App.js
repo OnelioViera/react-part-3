4cca5aa1514f215c661d92b2e420ba2fe2bfcae0
ojvwe <ojvwebdesign@gmail.com>
2023-05-25 09:27:50 -0400

    updated files

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ function App() {
                 age={38}
                 hobbies={["bowling", "watching tv", "drinking beer"]}
             />
-            <Person name="Marge" age={34} hobbies={["painting", "gambling"]} />
+            <Person name="Marge" age={34}
+                hobbies={["painting", "gambling"]} />
             <Person
                 name="Bart"
                 age={10}
--- Person.js
+++ Person.js
@@ -1,24 +1,53 @@
-function Person({ age, hobbies, name }) {
-    const voteText = age >= 18
-        ? "Go vote!"
-        : "Go study!";
+// function Person({ age, hobbies, name }) {
+//     const voteText = age >= 18
+//         ? "Go vote!"
+//         : "Go study!";
 
-    // your browser will issue a stern warning about this; we'll learn how to fix
-    // that later.
-    const hobbiesLIs = hobbies.map(hobby => <li>{hobby}</li>);
+//     // your browser will issue a stern warning about this; we'll learn how to fix
+//     // that later.
+//     const hobbiesLIs = hobbies.map(hobby => <li>{hobby}</li>);
+
+//     return (
+//         <div>
+//             <p>Learn some information about this person:</p>
+//             <ul>
+//                 <li>Name: {name.slice(0, MAX_NAME_LENGTH_TO_SHOW)}</li>
+//                 <li>Age: {age}</li>
+//                 <ul>
+//                     Hobbies: {hobbiesLIs}
+//                 </ul>
+//             </ul>
+//             <h3>{voteText}</h3>
+//         </div>
+//     );
+// }
+
+function Person({ name, age, hobbies }) {
+    const MAX_NAME_LENGTH_TO_SHOW = 8;
+    const voteText = age >= 18 ? "Go vote!" : "Go study!";
+
+    const hobbiesLIs = hobbies.map((hobby, index) => <li key={index}>{hobby}</li>);
 
     return (
-        <div>
-            <p>Learn some information about this person:</p>
-            <ul>
-                <li>Name: {name.slice(0, MAX_NAME_LENGTH_TO_SHOW)}</li>
-                <li>Age: {age}</li>
-                <ul>
-                    Hobbies:
-                    {hobbiesLIs}
-                </ul>
-            </ul>
-            <h3>{voteText}</h3>
+        <div className="doc-body">
+            <div className="row px-4">
+                <div className="col-md-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <p>Learn some information about this person:</p>
+                            <ul>
+                                <li>Name: {name.slice(0, MAX_NAME_LENGTH_TO_SHOW)}</li>
+                                <li>Age: {age}</li>
+                                <li>Hobbies:</li>
+                                <ul>{hobbiesLIs}</ul>
+                            </ul>
+                            <h3>{voteText}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 }
+
